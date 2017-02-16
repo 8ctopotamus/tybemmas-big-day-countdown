8952b6f4c84da39beec2dd918d6ac577496941eb
Josh Naylor <8ctopotamus@gmail.com>
2017-02-16 15:40:24 -0600

    styled countdown

--- tybemma-bigdaycountdown.php
+++ tybemma-bigdaycountdown.php
@@ -11,7 +11,7 @@
 
 add_action('wp_enqueue_scripts', 'tybemmasbdcd_register_scripts');
 function tybemmasbdcd_register_scripts() {
-	wp_enqueue_style( 'simply-countdown-style', plugins_url( '/css/simplyCountdown.theme.default.css',  __FILE__ ));
+	wp_enqueue_style( 'simply-countdown-style', plugins_url( '/css/tybemmas-big-day-countdown.css',  __FILE__ ));
 	wp_enqueue_script( 'simply-countdown-script', plugins_url( '/js/simplyCountdown.js',  __FILE__ ), array(), '', true);
 	wp_enqueue_script( 'tybemmas-bdcd-main', plugins_url( '/js/tybemmas-bdcd-main.js',  __FILE__ ), array(), '', true);
 }
